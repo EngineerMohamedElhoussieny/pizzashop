59041d2b63284dbaf9b762bb4ec7ac77d9283f53
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-06-12 16:08:49 +0200

    createGlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color:blue;
+  }
+`
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <div>
       Hello PizzaShop
     </div>
+    </>
   );
 }
 
