d70e7cdad0bd7d2b174a7de4998c86a208dc6ee0
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-06-13 20:14:11 +0200

    CSS Transitions

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -14,17 +14,23 @@ export const FoodLabel=styled.div`
 `
 
 export const Food=styled(Title)`
-  height:100px;
-  padding:10px;
-  font-size:20px;
-  background-image:${({img})=>`url(${img});`}
-  background-position:center;
-  background-size:cover;
-  filter:contrast(75%);
-  border-radius:7px;
-  box-shadow:0px 0px 10px 0px grey;
-  &:hover{
-      cursor:pointer;
-      opacity:0.7;
-  }
+height: 100px; 
+padding: 10px; 
+font-size: 20px; 
+background-image: ${({ img }) => `url(${img});`} 
+background-position: center;
+background-size: cover;
+filter: contrast(75%); 
+border-radius: 7px; 
+margin-top: 5px; 
+transition-property: box-shadow margin-top filter; 
+transition-duration: .1s; 
+box-shadow: 0px 0px 2px 0px grey;
+&:hover {
+ cursor: pointer; 
+ filter: contrast(100%); 
+ margin-top: 0px; 
+ margin-bottom: 5px; 
+ box-shadow: 0px 5px 10px 0px grey;
+}
 `
