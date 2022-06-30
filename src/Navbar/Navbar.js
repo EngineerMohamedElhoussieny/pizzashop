d49c6eba4b64151afc0723ff7df0ebce0f83d1d0
m-elhoussieny <adammohamed457@yahoo.com>
2022-06-30 14:44:54 +0200

    change header name

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -18,7 +18,7 @@ text-shadow: 1px 1px 4px #380502;
 export function Navbar(){
     return <NavbarStyled>
         <Logo>
-        Sliceline{" "}
+        Pizza Shop{" "}
         <span role="img" aria-label="pizza slice">
           🍕
         </span>
